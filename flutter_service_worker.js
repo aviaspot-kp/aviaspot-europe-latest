de6fd5fcf30fd2a60cdb82cbf217f94a332a80f3
aviaspot-kp <kp@aviaspot.com>
2021-12-08 17:06:40 +0600

    asdasd

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -81,7 +81,7 @@ self.addEventListener("install", (event) => {
   self.skipWaiting();
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
-      return cache.addAll(
+      cache.addAll(
         CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
